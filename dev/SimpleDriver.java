f53db9049655158a6fbff529926484a063b7775e
ccorall <85907212+ccorall@users.noreply.github.com>
2021-07-23 06:17:13 -0700

    Add files via upload

--- dev/SimpleDriver.java
+++ dev/SimpleDriver.java
@@ -30,19 +30,13 @@ public class SimpleDriver{
 			
 			//If the login/register succeeds
 			if(valid){
-				//Create ArrayList to hold potential match profiles
-				ProfileList profiles;
 				Boolean loop = true;
 				long timestart = System.currentTimeMillis();
 				long timeend = System.currentTimeMillis();
 				long difference = timeend - timestart;
-				
-				user.editProfileSetting("profile_age",Integer.toString(new Random().nextInt(50)),false);
-				user.saveAll();
 
-				
 				//Interval to resend heartbeat message in seconds
-				int beat_interval = 5;
+				int beat_interval = 15;
 				
 				//Infinite loop to keep client connected
 				while(loop){
@@ -52,51 +46,6 @@ public class SimpleDriver{
 						//Reset time marker and send heartbeat
 						timestart = System.currentTimeMillis();
 						auth.sendHeartbeat();
-						//win.updateText("Heartbeat sent");
-						System.out.println("-------------------------------------");
-						//Retrieve profiles
-						//The 'true' option specifies only nearby matches
-						System.out.println("Active profiles near me: ");
-						profiles = ms.getMatches(true);
-						//If there are any profiles in the results, print them
-						if(profiles!=null){
-							Profile p = profiles.getNextProfile();
-							while(p != null){
-								p.print();
-								if(p.getAttribute("profile_fname",false).compareTo("amon")==0 && user.getProfile().getAttribute("profile_fname",false).compareTo("amon")!=0){
-									ms.createMatch(p,"right");
-								}
-								p = profiles.getNextProfile();
-							}
-						}
-						System.out.println("-------------------------------------");
-						System.out.println("Previously rated profiles: ");
-						profiles = ms.getMatchHistory();
-						//If there are any profiles in the results, print them
-						if(profiles!=null){
-							Profile p = profiles.getNextProfile();
-							while(p != null){
-								p.print();
-								p = profiles.getNextProfile();
-							}
-						}
-						System.out.println("-------------------------------------");
-						System.out.println("Mutually matched profiles: ");
-						profiles = ms.getMutualMatches();
-						//If there are any profiles in the results, print them
-						if(profiles!=null){
-							Profile p = profiles.getNextProfile();
-							while(p != null){
-								p.print();
-								p = profiles.getNextProfile();
-							}
-						}
-						System.out.println("-------------------------------------");
-					}else{
-						if(difference%1000==0){
-							//Line below counts down heartbeat timer
-							//System.out.println(Long.toString(10 - difference/1000) + " seconds to heartbeat...");
-						}
 					}
 					timeend = System.currentTimeMillis();
 					difference = timeend - timestart;
